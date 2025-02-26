4cf6bfa4bb22a78118ded009d0f1e73585de0bc5
charlenelau-20 <charlenelau0120@gmail.com>
2025-02-26 19:24:30 +0900

    Update and rename messy-react.ts to refactored-messy-react.ts

---
+++ problem3/refactored-messy-react.ts
@@ -0,0 +1,118 @@
+interface WalletBalance {
+  currency: string;
+  amount: number;
+  blockchain: string;
+}
+interface FormattedWalletBalance {
+  currency: string;
+  amount: number;
+  formatted: string;
+}
+interface Price {
+  [currency: string]: number;
+}
+
+class Datasource {
+  url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async getPrices(): Promise<Price> {
+    try {
+        const response = await fetch(this.url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch prices:", error);
+        return {};
+    }
+  }
+}
+
+interface Props {
+    className?: string;
+    pricesUrl?: string;
+    [key: string]: any;
+}
+const WalletPage: React.FC<Props> = (props: Props) => {
+  const { children, ...rest } = props;
+  const balances = useWalletBalances();
+	const [prices, setPrices] = useState({});
+
+  useEffect(() => {
+    const datasource = new Datasource("https://interview.switcheo.com/prices.json");
+    datasource.getPrices().then(prices => {
+      setPrices(prices);
+    }).catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+	const getPriority = (blockchain: any): number => {
+	  switch (blockchain) {
+	    case 'Osmosis':
+	      return 100
+	    case 'Ethereum':
+	      return 50
+	    case 'Arbitrum':
+	      return 30
+	    case 'Zilliqa':
+	      return 20
+	    case 'Neo':
+	      return 20
+	    default:
+	      return -99
+	  }
+	}
+
+  const sortedBalances = useMemo(() => {
+    return balances.filter((balance: WalletBalance) => {
+		  const balancePriority = getPriority(balance.blockchain);
+		  if (balancePriority > -99) {
+		     if (balance.amount <= 0) {
+		       return false;
+		     }
+		  }
+		  return true
+		}).sort((lhs: WalletBalance, rhs: WalletBalance) => {
+			const leftPriority = getPriority(lhs.blockchain);
+		  const rightPriority = getPriority(rhs.blockchain);
+		  if (leftPriority > rightPriority) {
+		    return -1;
+		  } else if (rightPriority > leftPriority) {
+		    return 1;
+		  }
+    });
+  }, [balances, prices]);
+
+  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    return {
+      ...balance,
+      formatted: balance.amount.toFixed(5)
+    }
+  })
+
+  const rows = formattedBalances.map((balance: FormattedWalletBalance, index: number) => {
+    const usdValue = prices[balance.currency] * balance.amount;
+    return (
+      <WalletRow
+        className={classes.row}
+        key={index}
+        amount={balance.amount}
+        usdValue={usdValue}
+        formattedAmount={balance.formatted}
+      />
+    )
+  })
+
+  return (
+    <div {...rest}>
+      {rows}
+    </div>
+  )
+}
+
